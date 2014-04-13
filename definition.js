a2d2f460df60f35c9642a440d01583f968362356
Ian Johnson <enjalot@gmail.com>
2014-04-12 17:45:52 -0700

    add definitions and pronounciations

---
+++ definition.js
@@ -0,0 +1,41 @@
+// Define our histogram component's controller code
+function Definition() {}
+// All components have an init function that runs before anything happens
+Definition.prototype.init = function() {
+  var model = this.model;
+  model.setNull("char", ""); // setNull only sets the value if the data doesnt already exist
+
+}
+
+Definition.prototype.create = function() {
+  var that = this;
+  var model = this.model;
+  model.on("change", "char", function() {
+    that.fetch();
+  })
+}
+
+Definition.prototype.fetch = function() {
+  var model = this.model;
+  var character = model.get("char");
+  if(!character["char"]) {
+    model.set("definition", []);
+    model.set("numDefinitions", 0);
+    return;
+  }
+  var s3url = "https://s3.amazonaws.com/pinyin/characters/charhash/";
+  var url = s3url + encodeURIComponent(character["char"]) + ".csv";
+  console.log("url", url);
+  d3.csv(url, function(err, data) {
+    if(err) {
+      model.set("error", err);
+      console.log("error", err);
+      return;
+    }
+    data.sort(function(a,b) {
+      return a["char"].length - b["char"].length
+    });
+    model.set("definition", data);
+    model.set("numDefinitions", data.length);
+  });
+}
